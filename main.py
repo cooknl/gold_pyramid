5d14f9a1f575e63e4129fafa3178ac7a0eb17dea
Nate <nathan.gs20@gmail.com>
2014-11-09 22:05:21 -0600

    Working with seeds and bitwise operators and shifting

--- main.py
+++ main.py
@@ -8,18 +8,39 @@ def count_gold(pyramid):
     """
     rows = len(pyramid)
 
-    #Assemble every path
+    #Seed every path as an int, since the bits of the integer will cover every available path
 
-    paths = [bin(x) for x in range((rows-1)**2)]#[[0 for x in range((rows-1))] for x in range((rows-1)**2)]
-    #Use format??
+    paths = [[int(x)] for x in range(2**(rows-1))]#[[0 for x in range((rows-1))] for x in range((rows-1)**2)]
 
-    #for row in range(rows):
+    #Use bitwise operators!
 
+    #Iterate over the seeds
+    for int_path in paths:
+        #Copy the seed to the seed variable
+        seed = int_path[0]
+        #Replace the seed with the first value from the pyramid
+        int_path[0] = pyramid[0][0]
+        #Initiate a column variable with 0. This will be used to keep up with the "left/right" choices
+        col = 0
+        #Iterate over the rows
+        for row in range(1,rows):
+            #Add the current least significant bit from the seed integer to the current value of the column
+            # "0" = left and "1" = right
+            col += seed & int(1)
+            #bitshift the seed for the next row
+            seed = seed >> 1
+            #Append the value from pyramid in the given row/col to the int_path
+            int_path.append(pyramid[row][col])
+
+            #print(pyramid[row])
+    sums = [sum(x) for x in paths]
+    max_sum = max(sums)
+    print(max_sum)
 
 
 
     #replace this for solution
-    return 0
+    return max_sum
 
 
 if __name__ == '__main__':
